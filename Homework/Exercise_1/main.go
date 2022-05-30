b53ecbfdcb94b76cddf8b44a7162f3e119991180
ViToN <vitnow2009@gmail.com>
2022-05-30 20:15:53 +0500

    add prog Exercise_1

--- Homework/Exercise_1/main.go
+++ Homework/Exercise_1/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Start")
+	var a int
+	var b int
+
+	fmt.Print("Введите длину и ширину прямоугольника -  a, b: ")
+	fmt.Scanln(&a, &b)
+
+	if a > 0 && b > 0 {
+		fmt.Println("Площадь прямоугольника", (a * b), "см")
+	} else {
+		fmt.Println("ВЫ ввели  некорректные данные. Попробуйте еще раз.")
+	}
 }
