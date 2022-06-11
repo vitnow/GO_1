ec10df0bd103f9f03e57fee048e1d3b73bbba5cc
ViToN <vitnow2009@gmail.com>
2022-06-11 16:43:08 +0500

    made homework 2_3

--- Homework/Exercise_3/main.go
+++ Homework/Exercise_3/main.go
@@ -1,41 +1,66 @@
 package main
 
-import "fmt"
-
-//import "math"
-
-// func main() {
-//     add(1, 2, 3)        // sum = 6
-//     add(1, 2, 3, 4)     // sum = 10
-//     add(5, 6, 7, 2, 3)  // sum = 23
-// }
-
-// func add(numbers ...int){
-//     var sum = 0
-//     for _, number := range numbers{
-//         sum += number
-//     }
-//     fmt.Println("sum = ", sum)
-// }
+import (
+	"fmt"
+	//	"math"
+)
 
 func main() {
 	var a int
 	fmt.Print("Введите число от - a: ")
 	fmt.Scanln(&a)
-	ggg(a)
+	wordEnd(a)
+
 }
 
-func ggg(a int) {
-	if a < 0 || a > 999 {
-		fmt.Println("Ошибка аргумента, данное значение не находися в интермале [0, 999].")
+func wordEnd(a int) {
+	var a100 = (a) % 100
+	var a101 = a100 / 10
+	var a10 = a100 % 10
+	var a1 = (a) / 100
+	var b int
+	var c int
+	var d int
+	//Проверяем и выставляем индекс для сотен
+	if a1 >= 5 && a1 <= 20 {
+		b = 2 // 5
+	} else if a1 > 1 && a1 < 5 {
+		b = 1 // 2
+	} else if a1 == 1 {
+		b = 0 // 1
 	} else {
-		fmt.Println("Данное знасение находися в интермале [0, 999].")
+		b = 2 // 5}
+	}
+	//Проверяем и выставляем индекс для десятков
+	if a101 >= 5 && a101 <= 20 {
+		c = 2 // 5
+	} else if a101 > 1 && a101 < 5 {
+		c = 1 // 2
+	} else if a101 == 1 {
+		c = 0 // 1
+	} else {
+		c = 2 // 5}
+	}
+	//Проверяем и выставляем индекс для единиц
+	if a10 >= 5 && a10 <= 20 {
+		d = 2 // 5
+	} else if a10 > 1 && a10 < 5 {
+		d = 1 // 2
+	} else if a10 == 1 {
+		d = 0 // 1
+	} else {
+		d = 2 // 5}
+	}
+
+	arrHuns := [3]string{"сотня", "сотни", "сотен"}
+	arrTens := [3]string{"десяток", "десятка", "десятков"}
+	arrUnits := [3]string{"единица", "единицы", "единиц"}
+
+	// Производим проверку что введено число
+	if a < 0 || a > 999 || (a1 == 0 && a101 == 0 && a10 == 0) {
+		fmt.Println("Ошибка ввода, данное значение не является числом или не находися в интермале [0, 999].")
+	} else {
+		fmt.Println(a1, arrHuns[b], a101, arrTens[c], a10, arrUnits[d])
 	}
 }
 
-//	return {
-//	  units: numb % 10,
-//	  dozens: Math.floor(numb / 10) % 10,
-//	  hundreds: Math.floor(numb / 100) % 10,
-//	  thousands: Math.floor(numb / 1000),
-//	};
