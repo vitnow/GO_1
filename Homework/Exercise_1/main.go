c7f3465fa281ebf6d3ef45fce19e24130228682c
ViToN <vitnow2009@gmail.com>
2022-06-12 12:36:35 +0500

    add basic operations

--- Homework/Exercise_1/main.go
+++ Homework/Exercise_1/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!!!")
-}
+	var a, b, res float32
+	var op string
+	fmt.Print("Введите первое число: ")
+	fmt.Scanln(&a)
+	fmt.Print("Введите второе число: ")
+	fmt.Scanln(&b)
+	fmt.Print("Введите арифметическую операцию (+, -, *, /): ")
+	fmt.Scanln(&op)
+
+	switch op {
+	case "+":
+	res = a + b
+	case "-":
+	res = a - b
+	case "*":
+	res = a * b
+	case "/":
+	res = a / b
+	default:
+	fmt.Println("Операция выбрана неверно")
+	os.Exit(1)
+	}
+	fmt.Printf("Результат выполнения операции: %f\n", res)
+	}
