339874c6a9d681ecc491aede0c5ae15c89065b78
ViToN <vitnow2009@gmail.com>
2022-06-14 01:37:26 +0500

    add readme.md

--- Homework/main.go
+++ Homework/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+//	"math/rand"
+)
 
 func main() {
-	fmt.Println("Hello World!!!")
+// for i :=1; i < 40; i++{
+// 	min := -100
+//     max := 100
+// 	fmt.Print(rand.Intn(max - min) + min)
+// }
+	
+	ars := []int{-19, -13, -53, -41, -19, 18, -75, 40, -44, 0, -6, 11, 62, -11, 28, -26, -89, -55, -63, 6, -5, -34, 28, -42, -53, 47,
+	13, -12, 90, -85, 41, -92, 87, -69, -71, 56, 37, -69, -15}
+	SortInsert(ars)
+	fmt.Println(ars)
+}
+
+func SortInsert(ars []int) {
+	for i := 1; i < len(ars); i++ {
+		x := ars[i]
+		j := i
+		for ; j >= 1 && ars[j-1] > x; j-- {
+			ars[j] = ars[j-1]
+		}
+		ars[j] = x
+	}
 }
