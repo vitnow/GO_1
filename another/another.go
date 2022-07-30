cfe76ae4530408e147680d0e5b552ac0e451c283
ViToN <vitnow2009@gmail.com>
2022-07-30 16:40:43 +0500

    Edit main.go

---
+++ another/another.go
@@ -0,0 +1,36 @@
+package another
+
+import "flag"
+
+type Config struct {
+	dbConfig
+	poolConfig
+}
+
+type dbConfig struct {
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBUser     string
+	DBPassword string
+}
+
+type poolConfig struct {
+	PoolMaxConns    int
+	PoolMinConns    int
+	GoroutinesCount int
+}
+
+func ReadConfig() *Config {
+	c := &Config{}
+	flag.StringVar(&c.DBHost, "host", "127.0.0.1", "DB host")
+	flag.StringVar(&c.DBPort, "port", "5432", "DB port")
+	flag.StringVar(&c.DBName, "db", "home_corp", "DB name")
+	flag.StringVar(&c.DBUser, "user", "user", "DB user")
+	flag.StringVar(&c.DBPassword, "password", "user", "DB password")
+	flag.IntVar(&c.PoolMaxConns, "max-conns", 8, "connection pool MaxConnections param")
+	flag.IntVar(&c.PoolMaxConns, "min-conns", 8, "connection pool MinConnections param")
+	flag.IntVar(&c.GoroutinesCount, "goroutines", 50, "number of goroutines to run")
+	flag.Parse()
+	return c
+}
