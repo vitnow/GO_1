4f069a033c53259afa374265aed789f454053cc6
ViToN <vitnow2009@gmail.com>
2022-08-08 00:16:30 +0500

    add fibonachiOptim

--- fibchi.go
+++ fibchi.go
@@ -1,11 +1,17 @@
 package fibchi
 
 func fibonachi(i int) (int) {
-	if i == 0 {
-		return 0
+	if i < 2 {
+		return i
 	}
-	if i == 1 {
-		return 1
-	}
+	
 	return fibonachi(i-1) + fibonachi(i-2)
 }
+
+func fibonachiOptim(n int, hash map[int]int) int {
+	if val, ok := hash[n]; ok {
+		return val } 
+	hash[n] = fibonachiOptim(n-1, hash)	+ fibonachiOptim(n-2, hash)
+
+	return hash[n]
+}	 
